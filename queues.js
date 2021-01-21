82cbf1da6947acd349b9363e15b179487bb83431
aimee-liang <aimeeeliang@gmail.com>
2021-01-21 14:36:24 -0500

    Working on queues in array implementation

--- queues.js
+++ queues.js
@@ -1,5 +1,15 @@
 class Node{
     constructor (value){
         this.value = value
+        this.next = null
+    }
+}
+
+/* initialize a new Queue where there is no first or last index, and there are no elements */
+class Queue{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
     }
 }
