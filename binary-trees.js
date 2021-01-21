f3aa8325cace7260798416306d9d7a355ac82fe7
aimee-liang <aimeeeliang@gmail.com>
2021-01-21 17:40:24 -0500

    Started find() method on BST

--- binary-trees.js
+++ binary-trees.js
@@ -33,6 +33,39 @@ class BinarySearchTree {
             return this
         } else {
             let currentHead = this.head /* variable created to keep track of head and compare values */
+            while (true){
+                if (value === currentHead.value) return undefined /* to prevent from infinite looping */
+                if (value < currentHead.value){
+                    if (currentHead.left === null) {
+                        currentHead.left = node
+                        return this
+                    } else {
+                        currentHead = currentHead.left /* this will update current so we can compare the value to left and insert later on */
+                    }
+                } else if (value > currentHead.value){
+                    if (currentHead.right === null){
+                        currentHead.right = node
+                        return this
+                    } else {
+                        currentHead = currentHead.right
+                    }
+                }
+            }
         }
     }
+    /* to find: 
+        Create a new node
+        Starting at root, check to see if there is a root
+            If not, we’re done searching
+            If there is, see if the value of the root is the value we’re looking for. If it is, we’re done!
+            If not, we need to compare and see if the value is greater than or less than our newly created node
+                If greater
+                    Check and see if there is a node to the right
+                    If there is, move to that node and repeat above steps
+                    If not, we’re done!
+                If lesser
+                    Check and see if there is a node to the left
+                    If there is, move to that node and repeat
+                    If not, we’re done!
+    */
 }
