ed68bc2a0749a6535c439c77f2d9d83b11c04f2c
aimee-liang <aimeeeliang@gmail.com>
2021-01-21 14:47:46 -0500

    Working on enqueue logic

--- queues.js
+++ queues.js
@@ -1,3 +1,4 @@
+/* create a new node with a value */
 class Node{
     constructor (value){
         this.value = value
@@ -12,4 +13,18 @@ class Queue{
         this.last = null;
         this.size = 0;
     }
+    /* This function accepts some value
+        Create a new node with the value passed into the function
+        If there are no nodes, set this node to be first and last property of the queue
+        Otherwise, set the next property on the current last to be that node, and set the last property on the queue to be that node
+        Increment size by 1
+    */
+    enqueue(value){
+        let node = new Node(value)
+    }
+    
+    dequeue(){
+
+    }
 }
+
