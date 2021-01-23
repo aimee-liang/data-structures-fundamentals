105c0766e540f3f8ad0d2d402c19e640125b2af8
aimee-liang <aimeeeliang@gmail.com>
2021-01-23 17:38:39 -0500

    Working on remove()

--- binary-heaps.js
+++ binary-heaps.js
@@ -32,5 +32,18 @@ class MaxBinaryHeap{
                 this.values[element] = parentElement
                 index = parentIndex
             }
-        }
+    }
+    /* Remove/Extract
+    Swap the first value in the values property with the last one
+    Pop from the values property, so you can return value at the end
+    Have the new root “sink down” to the correct spot…
+        Your parent index starts at 0 (the root)
+        Find the index of the left child (2 * index + 1)
+        Find the index of the right child (2 * index + 2)
+        If the left or right child is greater than the element, swap it! If both left and right children are larger, swap with the largest child
+        The child index you swapped to now is the parent index
+        Keep looping and swapping until neither child is larger than the element
+        Return the old root
+    */
+    
 }
