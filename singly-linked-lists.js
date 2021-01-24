763e10411e7256b6e3006bce1c488ed9fa591a8d
aimee-liang <aimeeeliang@gmail.com>
2021-01-24 15:00:12 -0500

    Worked on push()

--- singly-linked-lists.js
+++ singly-linked-lists.js
@@ -11,5 +11,25 @@ class SinglyLinkedList{
         this.tail = null
         this.length = 0
     }
-    push()
+    /* to push
+    Function accepts a value
+    Create a new node using the value passed to the function
+    If there is no head property in the SLL, 
+        set the head and tail to be the newly created node
+        Otherwise, set the next property on the tail to be the new node, and the tail on the list to be the newly created node
+    Increment length by 1
+    Return linked list
+    */
+    push(value){
+        let node = new Node(value)
+        if (!this.head){
+            this.head = node
+            this.tail = node
+        } else {
+            this.tail.next = node
+            this.tail = node
+        }
+        ++this.length
+        return this
+    }
 }
