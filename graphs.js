99f11b28b51d76f0a9ea12394503c19604736117
aimee-liang <aimeeeliang@gmail.com>
2021-01-27 20:51:09 -0500

    Set name of vertex to empty array

--- graphs.js
+++ graphs.js
@@ -3,6 +3,6 @@ class Graph{
         this.adjacencyList = {} /* stores edges */
     }
     addVertex(name){
-        
+        this.adjacencyList[name] = [] /* set name of vertex to an empty array */
     }
 }
