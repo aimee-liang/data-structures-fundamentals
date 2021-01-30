deebde405e1268820b4192bfc6e62e70fed68ee4
aimee-liang <aimeeeliang@gmail.com>
2021-01-29 19:07:12 -0500

    Pseudocode for DFS Iteratively

--- graph-traversal.js
+++ graph-traversal.js
@@ -14,10 +14,35 @@ class Graph{
     DFSR(startingNode){
         let result = []
         let visited = {}
+        const adjacencyList = this.adjacencyList
         (function helper(vertex){
             if (!vertex) return
             visited[vertex] = true
             result.push(vertex)
+            adjacencyList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]){
+                    return helper(neighbor)
+                }
+            })
         })(startingNode)
+        return result
+    }
+    /* DFS - iterative
+    Function takes a node as a parameter
+    Create a stack to keep track of vertices
+    Create a list to store the end result, to be returned at the very end
+    Add the starting vertex to the stack, and mark it visited
+    While the stack has something in it,
+    Pop the next vertex from the stack
+    If the vertex hasn’t been visited yet
+    Mark it as visited
+    Add it to the result list
+    Push all of its neighbors to the stack
+    Return result array
+    */
+    DFSI(node){
+        let visited = []
+        let result = {}
+        
     }
 }
